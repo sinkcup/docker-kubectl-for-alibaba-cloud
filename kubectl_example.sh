3778bec8a17e63ed5f4f84f54588194858c362f0
sinkcup <sinkcup@gmail.com>
2019-05-05 20:23:49 +0800

    change image name

--- kubectl_example.sh
+++ kubectl_example.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 docker run -v "$HOME/.kube/config.example:/root/.kube/config" \
-    -it registry.cn-shanghai.aliyuncs.com/sinkcup/kubectl-vim:latest \
+    -it registry.cn-shanghai.aliyuncs.com/sinkcup/kubectl-vim-for-aliyun:latest \
     env LANG=C.UTF-8 \
     bash
